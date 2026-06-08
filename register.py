8fc8e983f3317eb79da4e6885abaf0f4744569fb
Raja R <raajarangasamy@gmail.com>
2026-06-08 10:04:36 +0530

    Added registration page UI

--- register.py
+++ register.py
@@ -1,2 +1,18 @@
-def register():
-    print("Registration Module")
+from tkinter import *
+
+root = Tk()
+root.title("Alumni Registration")
+root.geometry("400x300")
+
+Label(root, text="Name").pack()
+Entry(root).pack()
+
+Label(root, text="Email").pack()
+Entry(root).pack()
+
+Label(root, text="Password").pack()
+Entry(root, show="*").pack()
+
+Button(root, text="Register").pack(pady=10)
+
+root.mainloop()
