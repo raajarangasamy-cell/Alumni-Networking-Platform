92eefd209e9734e1cfc3c4f035878f07bdfb15c4
Raja R <raajarangasamy@gmail.com>
2026-06-08 10:06:30 +0530

    Added login page UI

--- login.py
+++ login.py
@@ -1,2 +1,16 @@
-def login():
+from tkinter import *
+
+root = Tk()
+root.title("Alumni Login")
+root.geometry("400x250")
+
+Label(root, text="Email").pack()
+Entry(root).pack()
+
+Label(root, text="Password").pack()
+Entry(root, show="*").pack()
+
+Button(root, text="Login").pack(pady=10)
+
+root.mainloop()def login():
     print("Login Module")
